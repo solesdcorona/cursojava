cb61c53d9f0f7c7891875e51b0c65bfb5b4bf26b
Rogelio Corona <rogelio.corona@axity.com>
2018-04-11 15:28:28 -0500

    prueba

---
+++ collections/src/main/java/curso/java/becario/App.java
@@ -0,0 +1,13 @@
+package curso.java.becario;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
